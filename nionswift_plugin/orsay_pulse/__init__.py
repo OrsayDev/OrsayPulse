7bc741cc1de86d3cab5537a39df71ecb8de4bef1
yvesauad <yvesauad@gmail.com>
2025-01-23 15:04:39 +0100

    v0.1 adding to the NS registry

--- nionswift_plugin/orsay_pulse/__init__.py
+++ nionswift_plugin/orsay_pulse/__init__.py
@@ -1,6 +1,8 @@
 from . import orsay_pulse_handler
 from . import orsay_pulse_tools
+from nion.utils import Registry
 
 def run():
     pulse_instrument = orsay_pulse_tools.PulseTools()
+    Registry.register_component(pulse_instrument, {"pulse_controller"})
     orsay_pulse_handler.run(pulse_instrument)
